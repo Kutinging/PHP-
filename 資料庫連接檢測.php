3015f30b98c68c725ca3fe5b02b4cbd04dea3cac
Kutinging <s14113103@stu.edu.tw>
2015-08-24 14:16:56 +0800

    Create 資料庫連接檢測.php

---
+++ 資料庫連接檢測.php
@@ -0,0 +1,18 @@
+<?php
+header('Content-type: text/html; charset=utf-8');  
+$userName=""; //資料庫帳號
+$password="";  //資料庫密碼
+$hostName=""; //資料庫主機(Server)名稱
+
+//建立資料連結
+if (!(@ $link=mysql_connect($hostName, $userName, $password)))
+{
+printf("<Br> 連結主機 %s 發生錯誤 <br>", $hostName);
+exit();
+}
+else
+{
+printf("<Br> 連結主機 %s 成功 <br>", $hostName);
+exit();
+}
+?>
