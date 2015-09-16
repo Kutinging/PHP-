18b9a43c5fe4744746d377bbee55ef54e9fff510
丁丁 <s14113103@stu.edu.tw>
2015-09-16 14:57:36 +0800

    Google Chart API 產生 QRCode

--- Google Chart API 產生 QRCode/index.php
+++ Google Chart API 產生 QRCode/index.php
@@ -1,23 +1,23 @@
 <?php
-	$info=$_POST["info"];
-	$wg=$_POST["wg"];
+	$info=$_POST["info"]; //取得info的值
+	$wg=$_POST["wg"]; //取得wg的值
 	echo "<img src=\"https://chart.googleapis.com/chart?chs=".$wg."&cht=qr&chl=" . $info . "&choe=UTF-8\">";
 ?>
 <html>
 	<head>
-		<title>Google Chart API����QRCode</title>
+		<title>Google Chart API產生QRCode</title>
 	</head>
 	<body>
 		<form method="post" action="">
-		<!--��ƥi�����}�Φr��-->
-			���X�n�s�񪺸�ơG<input type="text" name="info"><br/>
-		<!--QRCode���j�p�A�i�ۦ�s�W-->
-			�j�p�G
+		<!--資料可為網址或字串-->
+			條碼要存放的資料：<input type="text" name="info"><br/>
+		<!--QRCode的大小，可自行新增-->
+			大小：
 			<select type="text" name="wg">
 				<option value="100x100">100x100</option>
 				<option value="200x200">200x200</option>
 				<option value="300x300">300x300</option>
-			�@	<option value="400x400">400x400</option>
+			　	<option value="400x400">400x400</option>
 			</select>
 			<input type="submit">
 		</form>
